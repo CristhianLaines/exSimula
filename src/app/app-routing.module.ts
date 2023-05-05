be202ee58eb7978b6e8022174d45d8ad9b046d86
professor <pciscflo@upc.edu.pe>
2023-05-05 13:07:50 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/book/home/home.component';
+import { ListadoComponent } from './component/book/listado/listado.component';
+import { EditarComponent } from './component/book/editar/editar.component';
+import { BookComponent } from './component/book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'book', component: BookComponent, children: [
+      { path: 'listado', component : ListadoComponent},
+      { path: 'editar/:id', component: EditarComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './component/book/book.component';
+import { ListadoComponent } from './component/book/listado/listado.component';
+import { EditarComponent } from './component/book/editar/editar.component';
+import { NavbarComponent } from './component/book/navbar/navbar.component';
+import { HomeComponent } from './component/book/home/home.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    ListadoComponent,
+    EditarComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
